aa64b2042e053de91d4c5c5e4c22dda5d1205a60
shell <shellljx@gmail.com>
2016-12-22 17:24:48 +0800

    update sample

--- sample/src/main/java/com/licrafter/sample/MainActivity.java
+++ sample/src/main/java/com/licrafter/sample/MainActivity.java
@@ -3,12 +3,13 @@ package com.licrafter.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.licrafter.tagview.DIRECTION;
 import com.licrafter.tagview.TagViewGroup;
 import com.licrafter.tagview.views.TagView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TagViewGroup mTagGroup;
 
@@ -18,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTagGroup = (TagViewGroup) findViewById(R.id.tagGroup);
 
-        TagView view1 = new TagView(this);
-        view1.setText("我是右上");
-        view1.setDirection(DIRECTION.RIGHT_TOP_TILT);
-        mTagGroup.addTag(view1);
+        TagView v1 = new TagView(this);
+        v1.setText("我是右上");
+        v1.setDirection(DIRECTION.RIGHT_TOP_TILT);
+        mTagGroup.addTag(v1);
 
         TagView v2 = new TagView(this);
         v2.setText("我是右中啊啊");
@@ -49,15 +50,25 @@ public class MainActivity extends AppCompatActivity {
         v6.setDirection(DIRECTION.LEFT_TOP);
         mTagGroup.addTag(v6);
 
-        mTagGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mTagGroup.isHiden()) {
-                    mTagGroup.showWithAnimation();
-                } else {
-                    mTagGroup.hideWithAnimation();
-                }
+        v1.setOnClickListener(this);
+        v2.setOnClickListener(this);
+        v3.setOnClickListener(this);
+        v4.setOnClickListener(this);
+        v5.setOnClickListener(this);
+        v6.setOnClickListener(this);
+        findViewById(R.id.activity_main).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.activity_main) {
+            if (mTagGroup.isHiden()) {
+                mTagGroup.showWithAnimation();
+            } else {
+                mTagGroup.hideWithAnimation();
             }
-        });
+        }else {
+            Toast.makeText(this, ((TagView) v).getText().toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
