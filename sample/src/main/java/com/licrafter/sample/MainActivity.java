55e555be8c71f6e1286bd9afd02cbe8e4774bf28
shell <shellljx@gmail.com>
2016-12-23 21:54:10 +0800

    update sample

--- sample/src/main/java/com/licrafter/sample/MainActivity.java
+++ sample/src/main/java/com/licrafter/sample/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private TagViewGroup mTagGroup;
     private TagViewGroup mTagGroup2;
+    private TagViewGroup mTagGroup3;
+
+    private TagTextView v1, v2, v3, v4, v5, v6, v7, v8, v9, v0, v10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,41 +24,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         mTagGroup = (TagViewGroup) findViewById(R.id.tagGroup);
         mTagGroup2 = (TagViewGroup) findViewById(R.id.tagGroup2);
+        mTagGroup3 = (TagViewGroup) findViewById(R.id.tagGroup3);
+        initTagView();
 
-        TagTextView v1 = new TagTextView(this);
-        v1.setText("我是右上");
-        v1.setDirection(DIRECTION.RIGHT_TOP_TILT);
-
-        TagTextView v2 = new TagTextView(this);
-        v2.setText("我是右中啊啊");
-        v2.setDirection(DIRECTION.RIGHT_CENTER);
-
-        TagTextView v3 = new TagTextView(this);
-        v3.setText("这里是左下斜线嘿");
-        v3.setDirection(DIRECTION.LEFT_BOTTOM_TILE);
-
-        TagTextView v4 = new TagTextView(this);
-        v4.setText("右下");
-        v4.setDirection(DIRECTION.RIGHT_BOTTOM);
-
-
-        TagTextView v5 = new TagTextView(this);
-        v5.setText("这里是左中");
-        v5.setDirection(DIRECTION.LEFT_CENTER);
-
-        TagTextView v6 = new TagTextView(this);
-        v6.setText("我是左上哈哈哈哈黑哈嘿嘛咪!");
-        v6.setDirection(DIRECTION.LEFT_TOP);
         mTagGroup.setShowAnimator(AnimatorUtils.getTagShowAnimator(mTagGroup))
                 .setHideAnimator(AnimatorUtils.getTagHideAnimator(mTagGroup))
                 .addTag(v1).addTag(v2).addTag(v3).addTag(v4).addTag(v5).addTag(v6);
 
-        TagTextView v7 = new TagTextView(this);
-        v7.setText("我是右中，嗯没错");
-        v7.setDirection(DIRECTION.RIGHT_CENTER);
         mTagGroup2.setShowAnimator(AnimatorUtils.getTagShowAnimator(mTagGroup2))
                 .setHideAnimator(AnimatorUtils.getTagHideAnimator(mTagGroup2))
                 .addTag(v7);
+
+        mTagGroup3.setShowAnimator(AnimatorUtils.getTagShowAnimator(mTagGroup3))
+                .setHideAnimator(AnimatorUtils.getTagHideAnimator(mTagGroup3))
+                .addTag(v8).addTag(v9).addTag(v0).addTag(v10);
 
         v1.setOnClickListener(this);
         v2.setOnClickListener(this);
@@ -72,12 +54,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (mTagGroup.isHiden()) {
                 mTagGroup.showWithAnimation();
                 mTagGroup2.showWithAnimation();
+                mTagGroup3.showWithAnimation();
             } else {
                 mTagGroup.hideWithAnimation();
                 mTagGroup2.hideWithAnimation();
+                mTagGroup3.hideWithAnimation();
             }
         } else {
             Toast.makeText(this, ((TagTextView) v).getText().toString(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void initTagView() {
+        v1 = new TagTextView(this);
+        v1.setText("我是右上");
+        v1.setDirection(DIRECTION.RIGHT_TOP);
+
+        v2 = new TagTextView(this);
+        v2.setText("我是右中啊啊");
+        v2.setDirection(DIRECTION.RIGHT_CENTER);
+
+        v3 = new TagTextView(this);
+        v3.setText("这里是左下斜线嘿");
+        v3.setDirection(DIRECTION.LEFT_BOTTOM);
+
+        v4 = new TagTextView(this);
+        v4.setText("右下");
+        v4.setDirection(DIRECTION.RIGHT_BOTTOM);
+
+
+        v5 = new TagTextView(this);
+        v5.setText("这里是左中");
+        v5.setDirection(DIRECTION.LEFT_CENTER);
+
+        v6 = new TagTextView(this);
+        v6.setText("我是左上哈哈哈哈黑哈嘿嘛咪!");
+        v6.setDirection(DIRECTION.LEFT_TOP);
+
+        v7 = new TagTextView(this);
+        v7.setText("我是右中，嗯没错");
+        v7.setDirection(DIRECTION.RIGHT_CENTER);
+
+        v8 = new TagTextView(this);
+        v8.setText("三年血赚！");
+        v8.setDirection(DIRECTION.RIGHT_TOP_TILT);
+
+        v9 = new TagTextView(this);
+        v9.setText("枪毙不亏。");
+        v9.setDirection(DIRECTION.RIGHT_BOTTOM_TILT);
+
+        v0 = new TagTextView(this);
+        v0.setText("萝莉控");
+        v0.setDirection(DIRECTION.LEFT_TOP_TILT);
+
+        v10 = new TagTextView(this);
+        v10.setText("蓝白条胖次最好了，嗯");
+        v10.setDirection(DIRECTION.LEFT_BOTTOM_TILT);
     }
 }
