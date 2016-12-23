a534b76a11fa247792472e962c8b048f8b4a96d6
shell <shellljx@gmail.com>
2016-12-23 11:55:38 +0800

    update sample

--- sample/src/main/java/com/licrafter/sample/MainActivity.java
+++ sample/src/main/java/com/licrafter/sample/MainActivity.java
@@ -12,17 +12,19 @@ import com.licrafter.tagview.views.TagTextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TagViewGroup mTagGroup;
+    private TagViewGroup mTagGroup2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTagGroup = (TagViewGroup) findViewById(R.id.tagGroup);
+        mTagGroup2 = (TagViewGroup) findViewById(R.id.tagGroup2);
 
         TagTextView v1 = new TagTextView(this);
         v1.setText("我是右上");
         v1.setDirection(DIRECTION.RIGHT_TOP_TILT);
-        //mTagGroup.addTag(v1);
+        mTagGroup.addTag(v1);
 
         TagTextView v2 = new TagTextView(this);
         v2.setText("我是右中啊啊");
@@ -32,12 +34,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TagTextView v3 = new TagTextView(this);
         v3.setText("这里是左下斜线嘿");
         v3.setDirection(DIRECTION.LEFT_BOTTOM_TILE);
-       // mTagGroup.addTag(v3);
+        mTagGroup.addTag(v3);
 
         TagTextView v4 = new TagTextView(this);
         v4.setText("右下");
         v4.setDirection(DIRECTION.RIGHT_BOTTOM);
-       // mTagGroup.addTag(v4);
+        mTagGroup.addTag(v4);
 
 
         TagTextView v5 = new TagTextView(this);
@@ -48,7 +50,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TagTextView v6 = new TagTextView(this);
         v6.setText("我是左上哈哈哈哈黑哈嘿嘛咪!");
         v6.setDirection(DIRECTION.LEFT_TOP);
-        //mTagGroup.addTag(v6);
+        mTagGroup.addTag(v6);
+
+        TagTextView v7 = new TagTextView(this);
+        v7.setText("我是右中，嗯没错");
+        v7.setDirection(DIRECTION.RIGHT_CENTER);
+        mTagGroup2.addTag(v7);
 
         v1.setOnClickListener(this);
         v2.setOnClickListener(this);
@@ -64,10 +71,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v.getId() == R.id.activity_main) {
             if (mTagGroup.isHiden()) {
                 mTagGroup.showWithAnimation();
+                mTagGroup2.showWithAnimation();
             } else {
                 mTagGroup.hideWithAnimation();
+                mTagGroup2.hideWithAnimation();
             }
-        }else {
+        } else {
             Toast.makeText(this, ((TagTextView) v).getText().toString(), Toast.LENGTH_SHORT).show();
         }
     }
