1e43385f895961d008e1b41522c959e819ab0a9b
shell <shellljx@gmail.com>
2017-01-04 12:03:36 +0800

    update sample

--- sample/src/main/java/com/licrafter/sample/TagEditActivity.java
+++ sample/src/main/java/com/licrafter/sample/TagEditActivity.java
@@ -4,13 +4,14 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
 import com.licrafter.sample.model.TagGroupModel;
 import com.licrafter.sample.utils.DataRepo;
+import com.licrafter.sample.views.TagEditDialog;
 import com.licrafter.sample.views.TagImageView;
 import com.licrafter.tagview.DIRECTION;
 import com.licrafter.tagview.TagViewGroup;
@@ -23,12 +24,13 @@ import java.util.List;
  * author: shell
  * date 2016/12/30 下午9:08
  **/
-public class TagEditActivity extends AppCompatActivity implements View.OnClickListener, TagViewGroup.OnTagGroupClickListener {
+public class TagEditActivity extends AppCompatActivity implements View.OnClickListener
+        , TagEditDialog.OnTagEditDialogClickListener {
 
     private Button mSaveBtn, mCreateBtn;
     private TagImageView mTagImageView;
+    private TagEditDialog mDialog;
 
-    private EditText edit01, edit02, edit03;
     private int num;
     private List<TagGroupModel> mModelList = new ArrayList<>();
 
@@ -38,12 +40,10 @@ public class TagEditActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag_edit);
         mTagImageView = (TagImageView) findViewById(R.id.tagImageView);
-        edit01 = (EditText) findViewById(R.id.edit1);
-        edit02 = (EditText) findViewById(R.id.edit2);
-        edit03 = (EditText) findViewById(R.id.edit3);
+        mDialog = new TagEditDialog(this, this);
+        mCreateBtn = (Button) findViewById(R.id.createBtn);
 
         mSaveBtn = (Button) findViewById(R.id.saveButton);
-        mCreateBtn = (Button) findViewById(R.id.createButton);
         mTagImageView.setEditMode(true);
         mTagImageView.setImageUrl("http://ci.xiaohongshu.com/0c62c1d9-8183-4410-82cf-80492b88fdad@r_1280w_1280h.jpg");
         mSaveBtn.setOnClickListener(this);
@@ -60,78 +60,61 @@ public class TagEditActivity extends AppCompatActivity implements View.OnClickLi
                 break;
             case R.id.tagImageView:
                 break;
-            case R.id.createButton:
-                TagGroupModel model = createTagGroup();
-                mModelList.add(model);
-                mTagImageView.addTagGroup(model, this);
+            case R.id.createBtn:
+                mDialog.show();
                 break;
-
         }
     }
 
-    public TagGroupModel createTagGroup() {
-        TagGroupModel model = new TagGroupModel();
-        String str01 = edit01.getText().toString();
-        String str02 = edit02.getText().toString();
-        String str03 = edit03.getText().toString();
-        if (!TextUtils.isEmpty(str01)) {
-            TagGroupModel.Tag tag = new TagGroupModel.Tag();
-            tag.setName(str01);
-            tag.setDirection(DIRECTION.RIGHT_TOP.getValue());
-            model.getTags().add(tag);
+    private TagViewGroup.OnTagGroupClickListener mTagGroupClickListener = new TagViewGroup.OnTagGroupClickListener() {
+        @Override
+        public void onCircleClick(TagViewGroup group) {
+            Toast.makeText(TagEditActivity.this, "点击中心圆", Toast.LENGTH_SHORT).show();
         }
-        if (!TextUtils.isEmpty(str02)) {
-            TagGroupModel.Tag tag = new TagGroupModel.Tag();
-            tag.setName(str02);
-            tag.setDirection(DIRECTION.RIGHT_CENTER.getValue());
-            model.getTags().add(tag);
+
+        @Override
+        public void onTagClick(TagViewGroup group, ITagView tag, int index) {
+            tag.setDirection(DIRECTION.valueOf((num++ % 10 + 1)));
+            mModelList.get(mTagImageView.getTagGroupIndex(group)).getTags().get(index).setDirection(tag.getDirection().getValue());
+            group.requestLayout();
         }
-        if (!TextUtils.isEmpty(str03)) {
-            TagGroupModel.Tag tag = new TagGroupModel.Tag();
-            tag.setName(str03);
-            tag.setDirection(DIRECTION.RIGHT_BOTTOM.getValue());
-            model.getTags().add(tag);
+
+        @Override
+        public void onScroll(TagViewGroup group, float percentX, float percentY) {
+            mModelList.get(mTagImageView.getTagGroupIndex(group)).setPercentX(percentX);
+            mModelList.get(mTagImageView.getTagGroupIndex(group)).setPercentY(percentY);
         }
-        model.setPercentX(0.5f);
-        model.setPercentY(0.5f);
-        return model;
+
+        @Override
+        public void onLongPress(final TagViewGroup group) {
+            new AlertDialog.Builder(TagEditActivity.this)
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            mTagImageView.removeTagGroup(group);
+                            mModelList.remove(mTagImageView.getTagGroupIndex(group));
+                            dialog.dismiss();
+                        }
+                    }).setTitle("删除标签组").setMessage("你确定要删除该标签组吗？")
+                    .create().show();
+        }
+    };
+
+    @Override
+    public void onCancel() {
+        mDialog.dismiss();
     }
 
     @Override
-    public void onCircleClick(TagViewGroup group) {
-
-    }
-
-    @Override
-    public void onTagClick(TagViewGroup group, ITagView tag, int index) {
-        tag.setDirection(DIRECTION.valueOf((num++ % 10 + 1)));
-        mModelList.get(mTagImageView.getTagGroupIndex(group)).getTags().get(index).setDirection(tag.getDirection().getValue());
-        group.requestLayout();
-    }
-
-    @Override
-    public void onScroll(TagViewGroup group, float percentX, float percentY) {
-        mModelList.get(mTagImageView.getTagGroupIndex(group)).setPercentX(percentX);
-        mModelList.get(mTagImageView.getTagGroupIndex(group)).setPercentY(percentY);
-    }
-
-    @Override
-    public void onLongPress(final TagViewGroup group) {
-        new AlertDialog.Builder(TagEditActivity.this)
-                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        mTagImageView.removeTagGroup(group);
-                        mModelList.remove(mTagImageView.getTagGroupIndex(group));
-                        dialog.dismiss();
-                    }
-                }).setTitle("删除标签组").setMessage("你确定要删除该标签组吗？")
-                .create().show();
+    public void onTagGroupCreated(TagGroupModel group) {
+        mModelList.add(group);
+        mTagImageView.addTagGroup(group, mTagGroupClickListener);
+        mDialog.dismiss();
     }
 }
---
+++ sample/src/main/java/com/licrafter/sample/views/TagEditDialog.java
@@ -0,0 +1,104 @@
+package com.licrafter.sample.views;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.licrafter.sample.R;
+import com.licrafter.sample.model.TagGroupModel;
+import com.licrafter.tagview.DIRECTION;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * author: shell
+ * date 2017/1/4 上午10:46
+ **/
+public class TagEditDialog extends Dialog implements View.OnClickListener {
+
+    private EditText tag01, tag02, tag03;
+    private Button cancel, confirm;
+    private OnTagEditDialogClickListener listener;
+
+    public TagEditDialog(@NonNull Context context, OnTagEditDialogClickListener listener) {
+        super(context);
+        setContentView(R.layout.dialog_tag_edit);
+        this.listener = listener;
+        tag01 = (EditText) findViewById(R.id.tag01);
+        tag02 = (EditText) findViewById(R.id.tag02);
+        tag03 = (EditText) findViewById(R.id.tag03);
+        cancel = (Button) findViewById(R.id.cancelBtn);
+        confirm = (Button) findViewById(R.id.confirmBtn);
+        cancel.setOnClickListener(this);
+        confirm.setOnClickListener(this);
+    }
+
+    public TagGroupModel createTagGroup() {
+        TagGroupModel model = new TagGroupModel();
+        List<TagGroupModel.Tag> tagList = new ArrayList<>();
+        String str01 = tag01.getText().toString();
+        String str02 = tag02.getText().toString();
+        String str03 = tag03.getText().toString();
+        if (!TextUtils.isEmpty(str01)) {
+            TagGroupModel.Tag tag = new TagGroupModel.Tag();
+            tag.setName(str01);
+            tagList.add(tag);
+        }
+        if (!TextUtils.isEmpty(str02)) {
+            TagGroupModel.Tag tag = new TagGroupModel.Tag();
+            tag.setName(str02);
+            tagList.add(tag);
+        }
+        if (!TextUtils.isEmpty(str03)) {
+            TagGroupModel.Tag tag = new TagGroupModel.Tag();
+            tag.setName(str03);
+            tagList.add(tag);
+        }
+        setTagDirection(tagList);
+        model.getTags().addAll(tagList);
+        model.setPercentX(0.5f);
+        model.setPercentY(0.5f);
+        return model;
+    }
+
+    private void setTagDirection(List<TagGroupModel.Tag> tagList) {
+        switch (tagList.size()) {
+            case 3:
+                tagList.get(0).setDirection(DIRECTION.RIGHT_TOP.getValue());
+                tagList.get(1).setDirection(DIRECTION.RIGHT_CENTER.getValue());
+                tagList.get(2).setDirection(DIRECTION.RIGHT_BOTTOM.getValue());
+                break;
+            case 2:
+                tagList.get(0).setDirection(DIRECTION.RIGHT_TOP.getValue());
+                tagList.get(1).setDirection(DIRECTION.RIGHT_BOTTOM.getValue());
+                break;
+            case 1:
+                tagList.get(0).setDirection(DIRECTION.RIGHT_CENTER.getValue());
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cancelBtn:
+                listener.onCancel();
+                break;
+            case R.id.confirmBtn:
+                listener.onTagGroupCreated(createTagGroup());
+                break;
+        }
+    }
+
+    public interface OnTagEditDialogClickListener {
+        void onCancel();
+
+        void onTagGroupCreated(TagGroupModel group);
+    }
+}
